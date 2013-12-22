47b6a17504574c6305df12456cf855bd782c6ad6
Andrew Logan <andrewlogan@gmail.com>
2013-12-22 14:21:03 -0500

    Initial commit

---
+++ pynigma.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# pynigma.py
+#
+# Andrew Logan - 12/21/2013
+#
+# Enigma simulator written in python
+
+import sys
+
+def main(argv=None):
+    print "Hello World!"
+
+#Give main() a single exit point (see: http://www.artima.com/weblogs/viewpost.jsp?thread=4829)
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
